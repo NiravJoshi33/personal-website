a9e06d36573cda73426efb2a561d4363e5c9e13c
Nirav Joshi <niravjoshi3000@gmail.com>
2025-06-13 08:44:02 +0530

    added hero page

---
+++ src/lib/data.ts
@@ -0,0 +1,7 @@
+export const siteInfo = {
+  name: "Nirav Joshi - Personal Website",
+  description: "Nirav Joshi's personal website",
+  url: "https://niravjoshi.dev",
+  author: "Nirav Joshi",
+  authorEmail: "niravjoshi30@gmail.com",
+};
